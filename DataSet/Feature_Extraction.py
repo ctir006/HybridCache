064e6a61a7d9cb8144fff224257883c429eb884e
chittii <chittii@amazon.com>
2021-03-16 22:50:14 -0700

    Adding Data set csv file and feature extraction python code

---
+++ DataSet/Feature_Extraction.py
@@ -0,0 +1,31 @@
+#https://www.kaggle.com/nnqkfdjq/statistics-observation-of-random-youtube-video#count_observation_upload.csv
+
+import pandas as pd
+from collections import defaultdict
+import numpy as np
+dataset = pd.read_csv('count_observation_upload.csv')
+df = pd.DataFrame(dataset)
+cols = [2,13]
+df = df[df.columns[cols]]
+df.fillna(0, inplace=True)
+d=defaultdict(list)
+for i, row in enumerate(df.values[1:]):
+	d[row[0]].append(row[1])
+for i,key in enumerate(d):
+	if i!=0:
+		d[key]=d[key][1:]
+features=[]
+for i in d.values():
+	features.append(i)
+features=np.array(features)
+print(features.shape)
+print(type(features))
+features=features/features.sum(axis=0)
+features[features < 0] = 0
+for i in features:
+	print(i[:10])
+
+	
+	
+#[ 5881096. 15129395.  4786621.  8860295.  8254700. 11275645. 12121089. 12137170. 13875160. 12400016.]
+#features=np.around(features, decimals=5)
